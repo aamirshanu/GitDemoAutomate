6bf657d593a03462b9f2a623652c4812191a4d00
aamir <aamir.khan268268268@gmail.com>
2022-12-09 22:54:08 +0530

    commit from X

--- src/test/java/datadriven/New.java
+++ src/test/java/datadriven/New.java
@@ -7,5 +7,11 @@ public class New {
 	@Test
 	public void newData() {
 		System.out.println("this is new");
+		System.out.println("I have changed demo");
+	}
+	
+	public void newData2() {
+		System.out.println("this is new");
+		System.out.println("I have changed demo2");
 	}
 }
